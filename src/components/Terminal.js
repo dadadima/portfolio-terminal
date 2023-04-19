4970d98253186fd71f668d8c454e235d44695bca
Davide Di Matteo <davide.dimatteo@frontiersin.org>
2023-04-19 17:52:10 +0200

    reformatted

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,64 +1,64 @@
-import { useRef, useState, useEffect } from "react";
-import { CONTENTS } from "../utils/commandHelper";
+import {useEffect, useRef, useState} from "react";
+import {CONTENTS} from "../utils/commandHelper";
 import Command from "./Command";
 import styles from "./Terminal.module.css";
 
 export default function Terminal() {
-  const [commands, setCommands] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const terminalRef = useRef(null);
+    const [commands, setCommands] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const terminalRef = useRef(null);
 
-  const escapeHTML = (str) =>
-    str
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;");
+    const escapeHTML = (str) =>
+        str
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
 
-  const addCommand = async (command) => {
-    let output;
-    setLoading(true);
-    setCommands([...commands, { command, output: "Loading..." }]);
-    if (`${command}` in CONTENTS) {
-      output = await CONTENTS[`${command}`]();
-    } else if (command === "clear") {
-      setLoading(false);
-      return setCommands([]);
-    } else {
-      output = CONTENTS.error(escapeHTML(command));
-    }
+    const addCommand = async (command) => {
+        let output;
+        setLoading(true);
+        setCommands([...commands, {command, output: "Loading..."}]);
+        if (`${command}` in CONTENTS) {
+            output = await CONTENTS[`${command}`]();
+        } else if (command === "clear") {
+            setLoading(false);
+            return setCommands([]);
+        } else {
+            output = CONTENTS.error(escapeHTML(command));
+        }
 
-    setLoading(false);
-    setCommands([...commands.slice(0, commands.length), { command, output }]);
-    if (terminalRef) {
-      terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
-    }
-  };
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.ctrlKey && event.key === "l") {
-        event.preventDefault(); // Prevent the default browser action for Ctrl+L (e.g. focusing the address bar)
-        addCommand("clear");
-      }
+        setLoading(false);
+        setCommands([...commands.slice(0, commands.length), {command, output}]);
+        if (terminalRef) {
+            terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
+        }
     };
 
-    window.addEventListener("keydown", handleKeyDown);
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.ctrlKey && event.key === "l") {
+                event.preventDefault(); // Prevent the default browser action for Ctrl+L (e.g. focusing the address bar)
+                addCommand("clear");
+            }
+        };
 
-    return () => {
-      // Clean up the event listener when the component is unmounted
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [commands]);
+        window.addEventListener("keydown", handleKeyDown);
 
-  return (
-    <div className={styles.terminal} ref={terminalRef}>
-      {/* <Command command="help" output="Some very long text will go in here" /> */}
-      {commands.map(({ command, output }, index) => (
-        <Command command={command} output={output} key={index} />
-      ))}
-      {!loading && <Command onSubmit={(command) => addCommand(command)} />}
-    </div>
-  );
+        return () => {
+            // Clean up the event listener when the component is unmounted
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [commands]);
+
+    return (
+        <div className={styles.terminal} ref={terminalRef}>
+            {/* <Command command="help" output="Some very long text will go in here" /> */}
+            {commands.map(({command, output}, index) => (
+                <Command command={command} output={output} key={index}/>
+            ))}
+            {!loading && <Command onSubmit={(command) => addCommand(command)}/>}
+        </div>
+    );
 }
