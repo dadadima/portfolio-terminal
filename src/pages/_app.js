c5a1f22207b75689229676c05b6d566d8ed768bb
Davide Di Matteo <davide.dimatteo@frontiersin.org>
2023-05-02 01:27:22 +0200

    added favicon on head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Add favicon links here */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/robot-pink.ico"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
